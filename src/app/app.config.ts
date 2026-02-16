084bef01bb19a6cf1ed83bb0b32387d76fb815a2
sagarshresti18 <sagar18reshmi@gmail.com>
2026-02-16 08:33:28 +0530

    added the analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
 
 import { routes } from './app.routes';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCl0pPZCnLHIqBoi7X_J861fLJvcEKye9c",
+  authDomain: "taskflow-live.firebaseapp.com",
+  projectId: "taskflow-live",
+  storageBucket: "taskflow-live.firebasestorage.app",
+  messagingSenderId: "454504858738",
+  appId: "1:454504858738:web:48a089f5c7c765c8796eeb",
+  measurementId: "G-XWCRL01XYV"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAnalytics(() => getAnalytics())
   ]
 };
